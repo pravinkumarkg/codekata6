f344565a4ddbab54e338ee16844c4cc1bcb558d5
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-06 17:02:37 +0530

    Create kcheck.cpp

---
+++ kcheck.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,count=0;
+    cin>>n1>>n2;
+    int arr[n1];
+    for(int i=0;i<n1;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int j=0;j<n1;j++)
+    {
+        if(arr[j]==n2)
+        {
+            count++;
+        }
+    }
+    if(count>=1)
+    {
+    cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+    
+}
