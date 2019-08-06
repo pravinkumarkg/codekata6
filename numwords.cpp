03f65fd495f19394fcf55e96be3e23018401e269
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-06 15:54:19 +0530

    Create numwords.cpp

---
+++ numwords.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int n;
+   cin>>n;
+   switch(n)
+   {
+       case 1:
+       cout<<"One";
+       break;
+        case 2:
+       cout<<"Two";
+       break;
+        case 3:
+       cout<<"Three";
+       break;
+        case 4:
+       cout<<"Four";
+       break;
+        case 5:
+       cout<<"Five";
+       break;
+        case 6:
+       cout<<"Six";
+       break;
+        case 7:
+       cout<<"Seven";
+       break;
+        case 8:
+       cout<<"Eight";
+       break;
+        case 9:
+       cout<<"Nine";
+       break;
+       default:
+       cout<<"print no from 1 to 9";
+       
+   }
+   return 0;
+}
