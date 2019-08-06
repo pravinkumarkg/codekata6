46b1bbb3a370abd5b534a633a4c42563a90e6952
pravinkumarkg <35860045+pravinkumarkg@users.noreply.github.com>
2019-08-06 15:48:20 +0530

    Update digits.cpp

--- digits.cpp
+++ digits.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+using namespace std;
+int main() 
+{
+   
+   int n,sum=0,split;
+   cin>>n;
+   while(n>0)
+   {
+       split=n%10;
+       sum=sum*10+split;
+       n=n/10;
+   }
+   while(sum>0)
+   {
+       split=sum%10;
+        cout<<split<<" ";
+        sum=sum/10;
+   }
+   return 0;
+}
